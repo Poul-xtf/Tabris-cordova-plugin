d9eb1572ae885ccc4d49d09fa16bae0ed296c942
xutengfei <1572273890@qq.com>
2020-10-19 18:14:29 +0800

    2.0

--- www/device.js
+++ www/device.js
@@ -86,60 +86,14 @@
 
 
 class Calendar extends tabris.Widget {
-
-  get _nativeType() {
-    return 'com.eclipsesource.calendar.Calendar';
-  }
-
-  _listen(name, listening) {
-    if (name === 'dateChanged') {
-      this._nativeListen('dateChanged', listening);
-    } else {
-      super._listen(name, listening);
+    constructor(){
+        super.constructor()
     }
-  }
 
-  _trigger(name, event) {
-    if (name === 'dateChanged') {
-      this._triggerChangeEvent('date', parseInt(event.date));
-    } else {
-      return super._trigger(name, event);
-    }
+  _listen() {
+    return "nihaaaaa"
   }
 
 }
-
-tabris.NativeObject.defineProperties(Calendar.prototype, {
-  'date': {
-    type: 'number',
-    nocache: true,
-    encode(value) {
-      return value.toString();
-    },
-    decode(value) {
-      return parseInt(value);
-    }
-  },
-  'minDate': {
-    type: 'number',
-    nocache: true,
-    encode(value) {
-      return value.toString();
-    },
-    decode(value) {
-      return parseInt(value);
-    }
-  },
-  'maxDate': {
-    type: 'number',
-    nocache: true,
-    encode(value) {
-      return value.toString();
-    },
-    decode(value) {
-      return parseInt(value);
-    }
-  }
-});
 
 module.exports = Calendar;
